ef3c9f3bbb42e57544642669ab7d339089f173cf
Shashank Reddy <45118450+shshnkreddy@users.noreply.github.com>
2025-01-04 00:58:09 +0800

    Delete gym_carla directory

--- gym_carla/__init__.py
+++
@@ -1,8 +0,0 @@
-from gym_carla.envs import *
-
-# from gymnasium.envs.registration import register
-
-# register(
-#     id='carla-v0',
-#     entry_point='gym_carla.envs:CarlaEnv',
-# )
--- gym_carla/envs/__init__.py
+++
@@ -1,2 +0,0 @@
-from gym_carla.envs.carla_env import CarlaEnv
-from gym_carla.envs.wrapper import BirdEyeWrapper, DummyVecEnv, CameraWrapper
